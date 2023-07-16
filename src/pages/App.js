d392bb85e2fbcbe538a84165286d35b9dd324e3e
Alexandre Penneroux <penneroux.alexandre36@gmail.com>
2023-07-16 16:03:48 +0200

    maj app / css

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,13 +4,20 @@ import Footer from './footer/footer';
 import Header from './header/header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NotFound from './404/404';
+import Home from './home/home'
 
 
 function App() {
   return (
     <div className="App">
-      <Header />    
-        <NotFound/>
+      <Header />  
+        
+        <Router>
+            <Routes>
+                <Route path="/404" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
       <Footer />
     </div>
   );
---
+++ src/pages/home/home.js
@@ -0,0 +1,16 @@
+import './home.css'
+
+function Home(){
+
+    return(
+        <section>
+            <h1>C'est la page d'accueil</h1>
+        </section>
+      )
+}
+
+export default Home
+
+function getAppartement(){
+    
+}
